64d7b9cd8fde8c8c2085920dd005c87f6f8efe09
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-07-06 13:03:03 -0300

    feat: add expo-font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { View, Text } from 'react-native'
 import { ThemeProvider } from 'styled-components'
+import * as Font from 'expo-font'
+
+import { 
+	Poppins_400Regular,
+	Poppins_500Medium,
+	Poppins_700Bold
+} from '@expo-google-fonts/poppins'
 
 import theme from './src/global/styles/theme'
 import { Dashboard } from './src/screens/Dashboard'
 
 export default function App() {
+	const [appIsReady, setAppIsReady] = useState(false)
+
+	useEffect(() => {
+		async function prepare() {
+			try {
+				await Font.loadAsync({
+					Poppins_400Regular,
+					Poppins_500Medium,
+					Poppins_700Bold
+				})
+			} catch(e) {
+				console.warn(e)
+			} finally {
+				setAppIsReady(true)
+			}
+		}
+
+		prepare()
+	}, [])
+
+	if (!appIsReady) return <View><Text>Loading...</Text></View>
+
 	return (
 		<ThemeProvider theme={theme}>
 			<Dashboard />
